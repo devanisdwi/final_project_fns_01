3344bd040bba03a3387b26e65288db934f4d405f
zeeniye <bluemul07@gmail.com>
2022-12-14 11:07:59 +0800

    dags overview

---
+++ dags/fraud-online.py
@@ -0,0 +1,60 @@
+"""A liveness prober dag for monitoring composer.googleapis.com/environment/healthy."""
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    'fraud-online-pipeline',
+    schedule_interval="@daily",
+    default_args=default_args,
+    description='liveness monitoring dag',
+    schedule_interval=None,
+    dagrun_timeout=timedelta(minutes=20),
+    max_active_runs=1,
+    tags=['fns1-fp']
+) as dag:
+
+# priority_weight has type int in Airflow DB, uses the maximum.
+    t1 = BashOperator(
+        task_id='echo',
+        bash_command='echo test',
+        depends_on_past=False,
+        priority_weight=2**31 - 1,
+        do_xcom_push=False
+    )
+
+    download_dataset_task = BashOperator(
+        task_id="download_dataset_task",
+        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+    )
+
+    gcs_to_bigquery = BigQueryCreateExternalTableOperator(
+        task_id="bigquery_external_table_task",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": "external_table",
+            },
+            "externalDataConfiguration": {
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET_NAME}/raw/{PARQUET_FILE}"],
+            },
+        },
+    )
+
+    trigger_dbt_task = DbtCloudRunJobOperator(
+        task_id="trigger_job_run1",
+        job_id=48617,
+        check_interval=10,
+        timeout=300,
+    )
+
+    download_dataset_task >> gcs_to_bigquery >> trigger_dbt_task
