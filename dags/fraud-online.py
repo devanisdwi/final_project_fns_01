79e40a1c6ffde7bf799ac475f0d21aa278a51d1b
zeeniye <bluemul07@gmail.com>
2022-12-18 08:51:06 +0800

    dags success, download_to_gcs >> unzip_dataset >> format_to_parquet >> create_dataset >> gcs_to_bigquery

--- dags/fraud-online.py
+++ dags/fraud-online.py
@@ -8,7 +8,12 @@ from airflow.operators.python import PythonOperator
 # from google.cloud import storage
 # from google.oauth2 import service_account
 # https://stackoverflow.com/questions/55391105/location-of-home-airflow
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryCreateEmptyDatasetOperator,
+    BigQueryCreateExternalTableOperator,
+    BigQueryDeleteDatasetOperator,
+    BigQueryInsertJobOperator,
+)
 from datetime import timedelta
 # Imports the Google Cloud client library
 from google.cloud import storage
@@ -28,13 +33,13 @@ def format_to_parquet(src_file: str):
         logging.error('Can only accept source files in CSV format, for the moment')
         return
 
-    storage_client = storage.Client()
+    # storage_client = storage.Client()
 
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(f'{FILE_NAME}.csv')
-    blob.download_to_filename(f'{FILE_NAME}.csv')
+    # bucket = storage_client.bucket(BUCKET_NAME)
+    # blob = bucket.blob(f'{FILE_NAME}.csv')
+    # blob.download_to_filename(f'{FILE_NAME}.csv')
 
-    src_file = f'{FILE_NAME}.csv'
+    src_file = f'/home/airflow/gcs/data/{FILE_NAME}.csv'
     # fs = gcsfs.GCSFileSystem(project='foo')
     # with fs.open("bucket/foo/bar.csv", 'rb') as csv_file:
     #     pv.read_csv(csv_file)
@@ -74,9 +79,9 @@ with DAG(
     tags=['fns1-fp']
 ) as dag:
 
-    download_dataset_task = BashOperator(
-        task_id="download_dataset_task",
-        bash_command=f"kaggle datasets download rupakroy/online-payments-fraud-detection-dataset"
+    download_to_gcs_task = BashOperator(
+        task_id="download_to_gcs_task",
+        bash_command=f"kaggle datasets download rupakroy/online-payments-fraud-detection-dataset -p /home/airflow/gcs/data"
     )
 
     # check_ls_task = BashOperator(
@@ -91,7 +96,7 @@ with DAG(
 
     unzip_dataset_task = BashOperator(
         task_id="unzip_dataset_task",
-        bash_command=f"unzip online-payments-fraud-detection-dataset.zip"
+        bash_command=f"unzip /home/airflow/gcs/data/online-payments-fraud-detection-dataset.zip -d /home/airflow/gcs/data"
     )
 
     # format_to_parquet_task = PythonOperator(
@@ -106,19 +111,21 @@ with DAG(
         task_id="format_to_parquet_task",
         python_callable=format_to_parquet,
         op_kwargs={
-            "src_file": f"{FILE_NAME}.csv",
+            "src_file": f"/home/airflow/gcs/data/{FILE_NAME}.csv",
         },
     )
 
-    container_to_gcs_task = PythonOperator(
-        task_id="container_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET_NAME,
-            "object_name": f"abc/PS_20174392719_1491204439457_log.parquet",
-            "local_file": f"PS_20174392719_1491204439457_log.parquet",
-        },
-    )
+    create_dataset = BigQueryCreateEmptyDatasetOperator(task_id="create-dataset", dataset_id='dags_tests2_full')
+
+    # container_to_gcs_task = PythonOperator(
+    #     task_id="container_to_gcs_task",
+    #     python_callable=upload_to_gcs,
+    #     op_kwargs={
+    #         "bucket": BUCKET_NAME,
+    #         "object_name": f"abc/PS_20174392719_1491204439457_log.parquet",
+    #         "local_file": f"PS_20174392719_1491204439457_log.parquet",
+    #     },
+    # )
 
     gcs_to_bigquery = BigQueryCreateExternalTableOperator(
         task_id="bigquery_external_table_task",
@@ -126,11 +133,11 @@ with DAG(
             "tableReference": {
                 "projectId": "final-project-team1",
                 "datasetId": "final_project_test",
-                "tableId": "test_fraud_online",
+                "tableId": "fraud_online_success_full_dag",
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://coba-manual1412/abc/PS_20174392719_1491204439457_log.parquet"],
+                "sourceUris": [f"gs://us-central1-env-fns-b17e3bcb-bucket/data/PS_20174392719_1491204439457_log.parquet"],
             },
         },
     )
@@ -144,5 +151,5 @@ with DAG(
 
     # download_dataset_task 
     # >> check_ls_task
-    download_dataset_task >> unzip_dataset_task >> format_to_parquet_task >> container_to_gcs_task >> gcs_to_bigquery 
+    download_to_gcs_task >> unzip_dataset_task >> format_to_parquet_task >> create_dataset >> gcs_to_bigquery 
     # >> trigger_dbt_task
